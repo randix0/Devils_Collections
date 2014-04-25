f03634155e81764351bbc59af9768ae255ea9839
Yuriy Denyshchenko <ydenyshchenk@ebay.com>
2014-04-25 22:02:46 +0300

    subtopmenu

--- app/code/local/Devils/Collections/Block/Collections.php
+++ app/code/local/Devils/Collections/Block/Collections.php
@@ -1,5 +1,9 @@
 <?php
 class Devils_Collections_Block_Collections extends Mage_Core_Block_Template
 {
-
+    public function getCollections()
+    {
+        $collection = Mage::getModel('devils_collections/collections')->getCollection();
+        return $collection;
+    }
 }
